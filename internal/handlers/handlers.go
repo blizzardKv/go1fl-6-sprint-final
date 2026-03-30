66ae4e66646e516ff85a7a934bf71a705045ec11
Konstantin Pavlyuts <makemewanna@yandex.ru>
2026-03-30 08:08:39 +0300

    feat: add morze functional

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,71 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func GetIndex(w http.ResponseWriter, r *http.Request) {
+	content, err := os.ReadFile("index.html")
+	if err != nil {
+		http.Error(w, "Failed to read index.html", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write(content)
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20) // 10 MB
+	if err != nil {
+		http.Error(w, "Failed to parse form", http.StatusInternalServerError)
+		return
+	}
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Failed to get file from form", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Failed to read file", http.StatusInternalServerError)
+		return
+	}
+
+	result, err := service.Convert(string(data))
+	if err != nil {
+		http.Error(w, "Failed to convert data", http.StatusInternalServerError)
+		return
+	}
+
+	timestamp := time.Now().UTC().String()
+	ext := filepath.Ext(handler.Filename)
+	filename := timestamp + ext
+
+	outFile, err := os.Create(filename)
+	if err != nil {
+		http.Error(w, "Failed to create output file", http.StatusInternalServerError)
+		return
+	}
+	defer outFile.Close()
+
+	_, err = outFile.WriteString(result)
+	if err != nil {
+		http.Error(w, "Failed to write to output file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(result))
+}
